cafbd454e0b72bcba782e8a6f17f21861d2a2a9a
PerdonPs3 <you@example.com>
2025-08-26 11:00:00 -0400

    [+] Client Class

--- src/main/java/com/mycompany/tvsystem/Cliente.java
+++ src/main/java/com/mycompany/tvsystem/Cliente.java
@@ -9,5 +9,43 @@ package com.mycompany.tvsystem;
  * @author Maxi
  */
 public class Cliente {
+    private String nombre;
+    private String rut;
+    private String domicilio;
     
+    public Cliente(){
+        nombre = "";
+        rut = "";
+        domicilio = "";
+    }
+    
+    public Cliente(String nombre, String rut, String domicilio){
+        this.nombre = nombre;
+        this.rut = rut;
+        this.domicilio = domicilio;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setRut(String rut){
+        this.rut = rut;
+    }
+    
+    public void setDomicilio(String domicilio){
+        this.domicilio = domicilio;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getRut(){
+        return rut;
+    }
+    
+    public String getDomicilio(){
+        return domicilio;
+    }
 }
