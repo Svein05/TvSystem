ec70ee1fadf2a2644a77e930c2465ff1dd5b9b39
Elias Manríquez <elias.manriquez.2005@gmail.com>
2025-08-26 10:46:27 -0400

    Carga Inicial

---
+++ src/main/java/com/mycompany/tvsystem/TvSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tvsystem;
+
+/**
+ *
+ * @author elias
+ */
+public class TvSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
