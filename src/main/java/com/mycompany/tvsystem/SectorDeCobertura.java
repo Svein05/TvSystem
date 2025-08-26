03081f102349cd182b32ecb9695e020148a46263
PerdonPs3 <maxi.rodriguezpavez@gmail.com>
2025-08-26 11:03:54 -0400

    [+] Class Coverage Sector

--- src/main/java/com/mycompany/tvsystem/SectorDeCobertura.java
+++ src/main/java/com/mycompany/tvsystem/SectorDeCobertura.java
@@ -4,10 +4,30 @@
  */
 package com.mycompany.tvsystem;
 
-/**
- *
- * @author Maxi
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class SectorDeCobertura {
+    private String nombre;
+    private List<Cliente> clientes;
+    
+    public SectorDeCobertura(){
+        nombre = "";
+        this.clientes = new ArrayList<>();
+    }
+    
+    public SectorDeCobertura(String nombre){
+        this.nombre = nombre;
+        this.clientes = new ArrayList<>();
+    }
+    
+    public void addClient(Cliente cliente){
+        this.clientes.add(cliente);
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
     
 }
