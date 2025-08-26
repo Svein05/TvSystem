2be68099f5e37743cab32e00225ea4da7c69ef78
Your Name <you@example.com>
2025-08-26 10:54:17 -0400

    [+] Client Class

--- src/main/java/com/mycompany/tvsystem/Cliente.java
+++ src/main/java/com/mycompany/tvsystem/Cliente.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.tvsystem;
 
 /**
  *
  * @author Maxi
  */
-public class TvSystem {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Cliente {
+    
 }
